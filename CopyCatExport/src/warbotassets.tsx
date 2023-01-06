c8102c39e72ae77a27cdde2df22eaf354a9d8935
Rain <113399463+RainResearchLab@users.noreply.github.com>
2023-01-06 17:40:06 -0500

    Update warbotassets.tsx

--- CopyCatExport/src/warbotassets.tsx
+++ CopyCatExport/src/warbotassets.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./App.css";
+import "./warbotsassets.css";
 import iconlyLightBag from "./assets/iconlyLightBag.svg";
 import iconlyLightPlus from "./assets/iconlyLightPlus.svg";
 import iconlyLightActivity from "./assets/iconlyLightActivity.svg";
